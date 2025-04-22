9f5c5d77787ffb6809bbb01f1722d4e127554006
Artem Avdyushkin <tofurage@bk.ru>
2025-04-22 18:37:47 +0300

    Add admins site

--- MainApp/models.py
+++ MainApp/models.py
@@ -20,10 +20,18 @@ class Snippet(models.Model):
 
     def __repr__(self):
         return f'Snippet({self.name}, {self.lang})'
+    
+    
+    def __str__(self):
+        return f'{self.name}'
 
 class Comment(models.Model):
     text = models.TextField(max_length=1000)
     creation_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(to=User, on_delete=models.CASCADE)
     snippet = models.ForeignKey(to=Snippet, on_delete=models.CASCADE, related_name='comments')
+
+
+    def __str__(self):
+        return f'{self.text}'
 
